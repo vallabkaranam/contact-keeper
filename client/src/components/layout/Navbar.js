fe0943ba2b365d5f262674e9654151d78702cb15
Vallab Karanam <k.vallab6@gmail.com>
2021-12-24 11:16:07 -0800

    moved loadUser call to everpresent navbar  and reduced lifetime of token

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import AuthContext from "../../context/auth/authContext";
@@ -8,8 +8,13 @@ const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
   const contactContext = useContext(ContactContext);
 
-  const { isAuthenticated, logout, user } = authContext;
+  const { isAuthenticated, logout, user, loadUser } = authContext;
   const { clearContacts } = contactContext;
+
+  useEffect(() => {
+    loadUser();
+    // eslint-disable-next-line
+  }, []);
 
   const onLogout = () => {
     logout();
